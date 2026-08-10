88273c08f6fa18ac2550a8db0cb2a70ab93134e4
Himanshu pandey <pandeyhimanshu4111@gmail.com>
2026-08-10 23:37:37 +0530

    Add arithmetic operators example

---
+++ 2.0perators.js/1-Arithmetic_operators.js
@@ -0,0 +1,16 @@
+let a = 5;
+let b = 2;
+
+console.log("the sum is = ",a+b); // sum (+)
+console.log("the subtraction is = ",a-b); // subtraction (-)
+console.log("the multiplication is = ",a*b); // multiplication (*)
+console.log("the divide  is = ",a/b); // divide (/)
+console.log("the remainder is = ",a%b); // modulus (%)
+console.log("the exponent is = ",a**b); // exponentiation (**)
+
+
+
+
+
+
+
