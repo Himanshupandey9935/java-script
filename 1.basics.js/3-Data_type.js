62427a967d59776c3230725909a6c621b4074165
Himanshu pandey <pandeyhimanshu4111@gmail.com>
2026-08-10 23:36:03 +0530

    Add program demonstrating JavaScript data types

---
+++ 1.basics.js/3-Data_type.js
@@ -0,0 +1,10 @@
+//Non primitive type of data that works with object
+
+const student = {      // object name 
+    name : "himanshu", // himanshu = value and name = key 
+    age : 18,
+    Cgpa : 8.2,
+    isPass : true ,
+
+};
+console.log(student);
