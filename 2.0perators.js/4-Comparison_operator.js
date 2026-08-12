5c5dc125c905f167fa30412585e1a96624690028
Himanshu pandey <pandeyhimanshu4111@gmail.com>
2026-08-12 23:51:26 +0530

    Add Comparison operator example

---
+++ 2.0perators.js/4-Comparison_operator.js
@@ -0,0 +1,9 @@
+// Comparison operator example 
+
+let a = 2;
+let  b = "2";
+
+console.log(a==b); // equal to
+console.log(a===b); // triple equal to its also the data type
+console.log(a!=b); // not equal to 
+console.log(a!==b); // not triple equal to
