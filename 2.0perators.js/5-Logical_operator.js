ff84b526507aca675dde73e4edd9e6d7f2b7d966
Himanshu pandey <pandeyhimanshu4111@gmail.com>
2026-08-12 23:52:04 +0530

    Add Logical operator example

---
+++ 2.0perators.js/5-Logical_operator.js
@@ -0,0 +1,12 @@
+// logical operator examples
+
+let a = 7;
+let b = 6;
+console.log(a>b && a==7); /* and operator (&&) = when both 
+                         condition are true it give true */
+
+console.log(a>b || a==8); /* OR operator (||) = when any one  
+                         condition are true it give true */
+
+console.log(!a>b); /* NOT operator (!) = it always give 
+                  give opposite output */
