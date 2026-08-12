5658b9af27642dc24173335a927027fcc4d00842
Himanshu pandey <pandeyhimanshu4111@gmail.com>
2026-08-12 23:56:19 +0530

    Add grade Calculator  to assign student grades based on score ranges

---
+++ 3.Conditional_statement.js/Questions/1-Multiple_of_5.js
@@ -0,0 +1,9 @@
+// Check if the number is a multiple of 5 or not.
+let num = 15;
+
+if(num % 5 == 0){
+    console.log(num," is a multiple of 5");
+}
+else{
+    console.log(num, " is NOT a multiple of 5");
+}
