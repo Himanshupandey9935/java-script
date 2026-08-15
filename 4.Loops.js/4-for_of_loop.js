29319435444e809fe7daf3d8bb619fc689faf171
Himanshu pandey <pandeyhimanshu4111@gmail.com>
2026-08-15 20:02:58 +0530

    add for of loop example program

---
+++ 4.Loops.js/4-for_of_loop.js
@@ -0,0 +1,9 @@
+// example of for of loop 
+let size = 1;
+let string = "himanshu pandey";
+for(let val of string){
+    console.log("the value of string ", val);
+    size++;
+
+}
+console.log("the size of string",size);
