60e5b00f4fba2d0d476201676cd75814c5fdf975
Himanshu pandey <pandeyhimanshu4111@gmail.com>
2026-08-15 23:42:16 +0530

    Question- 3  Add number guessing game using loop

---
+++ 4.Loops.js/Questions/3-Guess_the_game.js
@@ -0,0 +1,13 @@
+/* QUESTION - 3 ,
+ Create a game where you start with a fixed game number 25.
+ Keep generating/trying different numbers until the
+ generated number matches the game number 25. */
+
+let fixNum = 25;
+let guess = 0;
+
+while(guess !== fixNum){
+    guess++;
+      console.log(guess);
+}
+console.log("congratulation")
